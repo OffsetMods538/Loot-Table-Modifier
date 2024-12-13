b8cbf5850d04e9f589b80d3bf334ae239e3c5898
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-12-13 13:51:50 +0200

    Initial Commit

---
+++ src/client/java/top/offsetmonkey538/loottablemodifier/LootTableModifierClient.java
@@ -0,0 +1,11 @@
+package top.offsetmonkey538.loottablemodifier;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LootTableModifierClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		// Do stuff
+	}
+}
---
+++ src/main/java/top/offsetmonkey538/loottablemodifier/LootTableModifier.java
@@ -0,0 +1,19 @@
+package top.offsetmonkey538.loottablemodifier;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LootTableModifier implements ModInitializer {
+	public static final String MOD_ID = "loot-table-modifier";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+
+	public static Identifier id(String path) {
+		return new Identifier(MOD_ID, path);
+	}
+}
