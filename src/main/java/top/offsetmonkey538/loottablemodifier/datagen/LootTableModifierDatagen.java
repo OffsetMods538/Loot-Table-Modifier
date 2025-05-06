13f4e9697432aeabe4f5b749d55ccdc082f084bc
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-05-06 15:54:05 +0300

    Remove original LootModifier

--- src/main/java/top/offsetmonkey538/loottablemodifier/datagen/LootTableModifierDatagen.java
+++ src/main/java/top/offsetmonkey538/loottablemodifier/datagen/LootTableModifierDatagen.java
@@ -13,7 +13,6 @@ import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.registry.RegistryWrapper;
-import top.offsetmonkey538.loottablemodifier.api.datagen.LootModifierProvider;
 import top.offsetmonkey538.loottablemodifier.api.datagen.NewLootModifierProvider;
 import top.offsetmonkey538.loottablemodifier.resource.action.AddPoolAction;
 
@@ -27,48 +26,7 @@ public class LootTableModifierDatagen implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         final FabricDataGenerator.Pack pack = fabricDataGenerator.createBuiltinResourcePack(id("example_pack"));
 
-        //pack.addProvider(ModLootModifierProvider::new);
         pack.addProvider(NewModLootModifierProvider::new);
-    }
-
-    private static class ModLootModifierProvider extends LootModifierProvider {
-        public ModLootModifierProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-            super(dataOutput, registriesFuture);
-        }
-
-        @Override
-        protected void generate(RegistryWrapper.WrapperLookup lookup) {
-            addModifier(
-                    id("drop_tnt"),
-
-
-                    LootPool.builder()
-                            .rolls(ConstantLootNumberProvider.create(1))
-                            .with(
-                                    ItemEntry.builder(Items.TNT)
-                                            .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1)))
-                            ),
-
-                    EntityType.CREEPER,
-                    EntityType.ZOMBIE
-            );
-
-            //todo: temp
-            addModifier(
-                    id("test"),
-
-                    LootPool.builder()
-                            .with(
-                                    ItemEntry.builder(Items.NETHERITE_SWORD).apply(
-                                            EnchantWithLevelsLootFunction
-                                                    .builder(lookup, UniformLootNumberProvider.create(20, 39))
-                                            //.builder(UniformLootNumberProvider.create(20, 39))
-                                    )
-                            ),
-
-                    LootTables.ABANDONED_MINESHAFT_CHEST
-            );
-        }
     }
 
     private static class NewModLootModifierProvider extends NewLootModifierProvider {
