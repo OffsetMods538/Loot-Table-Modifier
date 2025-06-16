cc1e8fade4729071697eeae091fc3c786643bbc9
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-06-16 15:57:27 +0300

    Force object version of OptionalPattern to always use regex

--- src/main/java/top/offsetmonkey538/loottablemodifier/resource/OptionalPattern.java
+++ src/main/java/top/offsetmonkey538/loottablemodifier/resource/OptionalPattern.java
@@ -5,16 +5,14 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public record OptionalPattern(boolean isRegex, @NotNull String patternString, @NotNull Pattern pattern) {
-    private static final Codec<OptionalPattern> INLINE_CODEC = Codec.STRING.xmap(OptionalPattern::new, OptionalPattern::patternString);
+    private static final Codec<OptionalPattern> INLINE_CODEC = Codec.STRING.xmap(OptionalPattern::literal, OptionalPattern::patternString);
     private static final Codec<OptionalPattern> FULL_CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            Codec.BOOL.optionalFieldOf("regex", false).forGetter(OptionalPattern::isRegex),
-            Codec.STRING.fieldOf("pattern").forGetter(OptionalPattern::patternString)
-    ).apply(instance, OptionalPattern::new));
+            Codec.STRING.fieldOf("regexPattern").forGetter(OptionalPattern::patternString)
+    ).apply(instance, OptionalPattern::compile));
     public static final Codec<OptionalPattern> CODEC = Codec.either(
             INLINE_CODEC,
             FULL_CODEC
@@ -23,32 +21,19 @@ public record OptionalPattern(boolean isRegex, @NotNull String patternString, @N
             pattern -> pattern.isRegex ? Either.right(pattern) : Either.left(pattern)
     );
 
-    private OptionalPattern(final String stringPattern) {
-        this(
+    public static OptionalPattern literal(final String literalString) {
+        return new OptionalPattern(
                 false,
-                stringPattern,
-                Pattern.compile(Pattern.quote(stringPattern))
+                literalString,
+                Pattern.compile(Pattern.quote(literalString))
         );
     }
 
-    private OptionalPattern(final boolean isRegex, final String stringPattern) {
-        this(
-                isRegex,
-                stringPattern,
-                Pattern.compile(isRegex ? stringPattern : Pattern.quote(stringPattern))
-        );
-    }
-
-    public static OptionalPattern compile(String patternString) {
+    public static OptionalPattern compile(String regexPattern) {
         return new OptionalPattern(
                 true,
-                patternString
-        );
-    }
-
-    public static OptionalPattern literal(String patternString) {
-        return new OptionalPattern(
-                patternString
+                regexPattern,
+                Pattern.compile(regexPattern)
         );
     }
 
