a69694866c52839d9209749d8ea08a38a7a1be82
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-07-17 19:54:20 +0300

    publish docs to seelf
    i really hope this works first try (it won't)
    https://github.com/OffsetMonkey538/seelf-config

---
+++ docs/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pnpm run build
+tar -czf archive.tar.gz dist Dockerfile docker-compose.yml nginx.conf
+curl -i -X POST -H "Authorization: Bearer $SEELF_TOKEN" -F environment=$SEELF_ENVIRONMENT -F archive=@archive.tar.gz $SEELF_APPLICATION_URL
