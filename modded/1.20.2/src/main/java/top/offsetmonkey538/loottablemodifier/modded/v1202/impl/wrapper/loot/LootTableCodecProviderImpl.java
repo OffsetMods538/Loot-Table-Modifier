76e20c3122d752ed3cce35e7fa28fc2d25933f1e
OffsetMonkey538 <offsetmonkey538@proton.me>
2026-01-09 20:51:56 +0200

    Implement loot element wrappers correctly for 1.20.2, delete GSONCodec for it as only 1.20.1 needs it

--- modded/1.20.2/src/main/java/top/offsetmonkey538/loottablemodifier/modded/v1202/impl/wrapper/loot/LootTableCodecProviderImpl.java
+++ modded/1.20.2/src/main/java/top/offsetmonkey538/loottablemodifier/modded/v1202/impl/wrapper/loot/LootTableCodecProviderImpl.java
@@ -1,17 +1,12 @@
 package top.offsetmonkey538.loottablemodifier.modded.v1202.impl.wrapper.loot;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.world.level.storage.loot.Deserializers;
 import top.offsetmonkey538.loottablemodifier.common.api.wrapper.loot.LootTable;
 import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.loot.LootTableWrapper;
-import top.offsetmonkey538.loottablemodifier.modded.v1202.codec.GSONCodec;
 
 public final class LootTableCodecProviderImpl implements LootTable.CodecProvider {
-    private static final Codec<LootTable> CODEC = new GSONCodec<>(Deserializers.createLootTableSerializer(), net.minecraft.world.level.storage.loot.LootTable.class)
-            .xmap(LootTableWrapper::new, wrappedPool -> ((LootTableWrapper) wrappedPool).vanillaTable());
-
     @Override
     public Codec<LootTable> get() {
-        return CODEC;
+        return net.minecraft.world.level.storage.loot.LootTable.CODEC.xmap(LootTableWrapper::new, wrappedPool -> ((LootTableWrapper) wrappedPool).vanillaTable());
     }
 }
--- modded/1.20.2/src/main/java/top/offsetmonkey538/loottablemodifier/modded/v1202/impl/wrapper/loot/entry/LootPoolEntryCodecProviderImpl.java
+++ modded/1.20.2/src/main/java/top/offsetmonkey538/loottablemodifier/modded/v1202/impl/wrapper/loot/entry/LootPoolEntryCodecProviderImpl.java
@@ -1,18 +1,13 @@
 package top.offsetmonkey538.loottablemodifier.modded.v1202.impl.wrapper.loot.entry;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.world.level.storage.loot.Deserializers;
-import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntries;
 import top.offsetmonkey538.loottablemodifier.common.api.wrapper.loot.entry.LootPoolEntry;
 import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.loot.entry.LootPoolEntryWrapper;
-import top.offsetmonkey538.loottablemodifier.modded.v1202.codec.GSONCodec;
 
 public final class LootPoolEntryCodecProviderImpl implements LootPoolEntry.CodecProvider {
-    private static final Codec<LootPoolEntry> CODEC = new GSONCodec<>(Deserializers.createFunctionSerializer(), LootPoolEntryContainer.class)
-            .xmap(LootPoolEntryWrapper::create, wrappedEntry -> ((LootPoolEntryWrapper) wrappedEntry).vanillaEntry);
-
     @Override
     public Codec<LootPoolEntry> get() {
-        return CODEC;
+        return LootPoolEntries.CODEC.xmap(LootPoolEntryWrapper::create, wrappedEntry -> ((LootPoolEntryWrapper) wrappedEntry).vanillaEntry);
     }
 }
