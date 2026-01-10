ce7bf1593440c110223d69e4a7f2bb40314c02df
OffsetMonkey538 <offsetmonkey538@proton.me>
2026-01-10 23:29:35 +0200

    mark correct supported versions and fix >=1.21.4 for neoforge

---
+++ loader/neoforge/1.21.4/src/main/java/top/offsetmonkey538/loottablemodifier/neoforge/v1214/mixin/ReloadableRegistriesMixin.java
@@ -0,0 +1,48 @@
+package top.offsetmonkey538.loottablemodifier.neoforge.v1214.mixin;
+
+import com.google.gson.JsonElement;
+import it.unimi.dsi.fastutil.Pair;
+import net.minecraft.core.Registry;
+import net.minecraft.core.WritableRegistry;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.server.ReloadableServerRegistries;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.level.storage.loot.LootDataType;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.offsetmonkey538.loottablemodifier.common.LootTableModifierCommon;
+import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.IdentifierWrapper;
+import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.ResourceManagerWrapper;
+import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.loot.LootTableWrapper;
+
+@Mixin(
+        value = ReloadableServerRegistries.class,
+        priority = 900
+)
+public abstract class ReloadableRegistriesMixin {
+    @Inject(
+            //method = "lambda$scheduleRegistryLoad$5(Lnet/minecraft/world/level/storage/loot/LootDataType;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/resources/RegistryOps;)Lnet/minecraft/core/WritableRegistry;",
+            method = "lambda$scheduleRegistryLoad$5(Lnet/minecraft/world/level/storage/loot/LootDataType;Lnet/minecraft/resources/RegistryOps;Lnet/minecraft/server/packs/resources/ResourceManager;)Lnet/minecraft/core/WritableRegistry;",
+            at = @At("RETURN")
+    )
+    private static <T> void loot_table_modifier$modifyLootTables(LootDataType<T> lootDataType, RegistryOps<JsonElement> registryOps, ResourceManager resourceManager, CallbackInfoReturnable<WritableRegistry<?>> cir) {
+        if (lootDataType != LootDataType.TABLE) return;
+
+        //noinspection unchecked
+        final Registry<LootTable> lootRegistry = (Registry<LootTable>) cir.getReturnValue();
+
+        LootTableModifierCommon.runModification(
+                new ResourceManagerWrapper(resourceManager),
+                lootRegistry
+                        .listElements()
+                        .map(registryEntry -> Pair.of(
+                                new IdentifierWrapper(registryEntry.key().location()),
+                                new LootTableWrapper(lootRegistry.getValue(registryEntry.key()))
+                        )),
+                registryOps
+        );
+    }
+}
