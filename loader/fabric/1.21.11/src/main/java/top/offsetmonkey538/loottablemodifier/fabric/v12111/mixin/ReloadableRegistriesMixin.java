34061bdceffee7fb71100dd21c96073c3d91e771
OffsetMonkey538 <offsetmonkey538@proton.me>
2026-01-18 16:23:32 +0200

    Add fabric 1.21.11 subproject

---
+++ loader/fabric/1.21.11/src/main/java/top/offsetmonkey538/loottablemodifier/fabric/v12111/mixin/ReloadableRegistriesMixin.java
@@ -0,0 +1,48 @@
+package top.offsetmonkey538.loottablemodifier.fabric.v12111.mixin;
+
+import com.google.gson.JsonElement;
+import it.unimi.dsi.fastutil.Pair;
+import net.minecraft.core.Registry;
+import net.minecraft.core.WritableRegistry;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.server.ReloadableServerRegistries;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.level.storage.loot.LootDataType;
+import net.minecraft.world.level.storage.loot.LootTable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.offsetmonkey538.loottablemodifier.common.LootTableModifierCommon;
+import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.ResourceManagerWrapper;
+import top.offsetmonkey538.loottablemodifier.modded.impl.wrapper.loot.LootTableWrapper;
+import top.offsetmonkey538.monkeylib538.modded.api.wrapper.ModdedIdentifier;
+
+@Mixin(
+        value = ReloadableServerRegistries.class,
+        priority = 900
+)
+public abstract class ReloadableRegistriesMixin {
+    @Inject(
+            // Works for fabric >=1.21.2
+            method = "method_61240(Lnet/minecraft/world/level/storage/loot/LootDataType;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/resources/RegistryOps;)Lnet/minecraft/core/WritableRegistry;",
+            at = @At("RETURN")
+    )
+    private static <T> void loot_table_modifier$modifyLootTables(LootDataType<T> lootDataType, ResourceManager resourceManager, RegistryOps<JsonElement> registryOps, CallbackInfoReturnable<WritableRegistry<?>> cir) {
+        if (lootDataType != LootDataType.TABLE) return;
+
+        //noinspection unchecked
+        final Registry<LootTable> lootRegistry = (Registry<LootTable>) cir.getReturnValue();
+
+        LootTableModifierCommon.runModification(
+                new ResourceManagerWrapper(resourceManager),
+                lootRegistry
+                        .listElements()
+                        .map(registryEntry -> Pair.of(
+                                ModdedIdentifier.of(registryEntry.key().identifier()),
+                                new LootTableWrapper(lootRegistry.getValue(registryEntry.key()))
+                        )),
+                registryOps
+        );
+    }
+}
